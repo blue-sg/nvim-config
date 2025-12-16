60bfe7d2afc2a362699b7b86c62a0d9105f5987a
blue-sg <blue-sg@github.com>
2025-12-16 16:18:29 +0300

    transparent

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -2,4 +2,8 @@ local config = {
 	cmd = {vim.fn.expand('~/.local/share/nvim/mason/bin/jdtls')},
 	root_dir = vim.fs.dirname(vim.fs.find({'gradlew','.git','mvnw'},{upward = true})[1]),
 }
-require('jdtls').start_or_attach(config)
+require('jdtls').start_or_attach(config)-- Enable DAP for Java
+jdtls.setup_dap()  -- this sets dap.adapters.java and dap.configurations.java automatically
+
+-- Optional: set a default main class to avoid being prompted
+
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,6 +2,61 @@
 -- See the kickstart.nvim README for more information
 
 return {
+	{
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  config = function()
+    require("toggleterm").setup{
+      size = 15,
+      open_mapping = [[<c-\>]],  -- Ctrl-\ opens terminal
+      hide_numbers = true,
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      direction = "float",        -- floating terminal
+      close_on_exit = true,
+      persist_size = true,
+      shell = vim.o.shell,
+    }
+
+    local Terminal  = require('toggleterm.terminal').Terminal
+    local float_term = Terminal:new({hidden = true, direction = "float"})
+
+    -- Toggle floating terminal manually
+    vim.keymap.set("n", "<F12>", function()
+      float_term:toggle()
+    end, {silent = true})
+
+    -- Function to run current file in floating terminal
+    function RunCurrentFile()
+      local ft = vim.bo.filetype
+      local file = vim.fn.expand("%:p")
+      local cmd = ""
+
+      if ft == "python" then
+        cmd = "python3 " .. file
+      elseif ft == "lua" then
+        cmd = "lua " .. file
+      elseif ft == "c" then
+        cmd = "gcc " .. file .. " -o /tmp/a.out && /tmp/a.out"
+      elseif ft == "cpp" then
+        cmd = "g++ " .. file .. " -o /tmp/a.out && /tmp/a.out"
+      elseif ft == "java" then
+        cmd = "javac " .. file .. " && java " .. vim.fn.expand("%:t:r")
+      elseif ft == "javascript" then
+        cmd = "node " .. file
+      else
+        cmd = "echo 'No run command for this filetype'"
+      end
+
+      float_term:toggle()
+      float_term:send(cmd)
+    end
+
+    -- Map key to run current file
+    vim.keymap.set("n", "<F5>", RunCurrentFile, {silent = true})
+  end
+},
   {
     "nvimtools/none-ls.nvim",
     dependencies = {
